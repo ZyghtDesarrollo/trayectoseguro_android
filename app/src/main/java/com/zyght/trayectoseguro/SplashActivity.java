f9967b2304f2c073d874c685106718eaa31609be
Arley <arley.duarte@zyght.com>
2017-03-20 17:56:52 -0300

    login trayecto seguro

---
+++ app/src/main/java/com/zyght/trayectoseguro/SplashActivity.java
@@ -0,0 +1,56 @@
+package com.zyght.trayectoseguro;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int   SPLASH_TIME_OUT= 3000;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+        showNextActivity();
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+
+    }
+
+    protected void showNextActivity() {
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
